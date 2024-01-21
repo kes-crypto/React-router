6a174643152341a997bb6011625009dd6c85eea7
Dennis Kestone <denniskestone@gmail.com>
2024-01-21 17:34:26 +0300

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Root, { loader as rootLoader } from "./routes/root";
+import ErrorPage from "./error-page";
+import ContactPage from "./routes/contact";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <ContactPage />,
+      },
+    ],
+    loader: rootLoader,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
